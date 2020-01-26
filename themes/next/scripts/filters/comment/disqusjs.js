1b6cbbd0376d012c87f118e9ed5eb6c552491b42
yudete <yudesleepy@gmail.com>
2020-01-26 13:34:59 +0900

    updated NeXT theme

---
+++ themes/next/scripts/filters/comment/disqusjs.js
@@ -0,0 +1,24 @@
+/* global hexo */
+
+'use strict';
+
+const path = require('path');
+
+// Add comment
+hexo.extend.filter.register('theme_inject', injects => {
+  let theme = hexo.theme.config;
+  if (!theme.disqusjs.enable || !theme.disqusjs.shortname || !theme.disqusjs.apikey) return;
+
+  injects.comment.raw('disqusjs', `
+  <div class="comments">
+    <div id="disqus_thread">
+      <noscript>Disqus のコメントを見るには、Javascript を有効にしてください。</noscript>
+    </div>
+  </div>
+  `, {}, {
+    cache: true
+  });
+
+  injects.bodyEnd.file('disqusjs', path.join(hexo.theme_dir, 'layout/_third-party/comments/disqusjs.swig'));
+
+});
